f6edfde8cfcd60102d402193f0176e0a9765658b
ADITHYAADHI06 <adishettygu102@gmail.com>
2023-08-28 09:51:26 +0530

    testing of contact page

--- __test__/contact.test.js
+++ __test__/contact.test.js
@@ -2,48 +2,52 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../src/components/Contact";
 import "@testing-library/jest-dom"
 
-test('Check Contact UI is Loaded or Not', () => {
-    render(<Contact />)
 
-    const heading = screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
-});
+Discription("test cases for contact page", () => {
 
-//button check
-test('Check Button is Loaded or Not', () => {
-    render(<Contact />)
+    test('Check Contact UI is Loaded or Not', () => {
+        render(<Contact />)
 
-    const button = screen.getByRole("button");
-    expect(button).toBeInTheDocument();
-});
+        const heading = screen.getByRole("heading");
+        expect(heading).toBeInTheDocument();
+    });
 
+    //button check
+    test('Check Button is Loaded or Not', () => {
+        render(<Contact />)
 
+        const button = screen.getByRole("button");
+        expect(button).toBeInTheDocument();
+    });
 
-//getByText
-test('Check Button is Loaded or Not with text', () => {
-    render(<Contact />)
+    //getByText
+    test('Check Button is Loaded ort Not with text', () => {
+        render(<Contact />)
 
-    const button = screen.getByText("Submit");
-    expect(button).toBeInTheDocument();
-});
-
-//placeholder
-test('Check input is Loaded or Not with text', () => {
-    render(<Contact />)
-    const input = screen.getByPlaceholderText("name");
-    expect(input).toBeInTheDocument();
-});
+        const button = screen.getByText("Submit");
+        expect(button).toBeInTheDocument();
+    });
 
 
-// 2 input box ==> multiple ==> use ALL
-test('Check 2 input is Loaded or Not with text', () => {
-    render(<Contact />)
 
-    const inputboxes = screen.getAllByRole("textbox");
+    //placeholder
+    test('Check input is Loaded or Not with text', () => {
+        render(<Contact />)
+        const input = screen.getByPlaceholderText("name");
+        expect(input).toBeInTheDocument();
+    });
 
-    expect(inputboxes.length).toBe(2);
+    // 2 input box ==> multiple ==> use ALL
+    test('Check 2 input is Loaded or Not with text', () => {
+        render(<Contact />)
 
-    // expect(inputboxes.length).not.toBe(3);
-});
+        const inputboxes = screen.getAllByRole("textbox");
+
+        expect(inputboxes.length).toBe(2);
+
+        // expect(inputboxes.length).not.toBe(3);
+    });
+})
+
 
 
